9759f7dbdadf23bedde1994a3f34c2beb90f0d06
Computer Science Guest 134 <zzz134@usask.ca>
2019-01-26 12:50:11 -0600

    movement is handled, and grabing is being worked on.

--- HouseBuilder/Assets/Scripts/Movement.cs
+++ HouseBuilder/Assets/Scripts/Movement.cs
@@ -8,27 +8,34 @@ public class Movement : MonoBehaviour {
 	public float vertical_movement_speed = 3.0f;
 	public Rigidbody player_physics;	
 	public BoxCollider player_collider;
-
-	Animator animation_manager;
+	bool canGrab = false;
 
 	// Use this for initialization
 	void Start () {
 		player_physics = this.GetComponent<Rigidbody> ();
+		Animator anim = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontal_input = Input.GetAxis ("Horizontal");
+		float horizontal_input = Input.GetAxis ("P1_Horizontal");
 		this.transform.position += new Vector3 (horizontal_input * horizontal_movement_speed * Time.deltaTime, 0, 0);
 
-		float vertical_input = Input.GetAxis ("Vertical");
+		float vertical_input = Input.GetAxis ("P1_Vertical");
 		this.transform.position += new Vector3 (0, vertical_input * vertical_movement_speed * Time.deltaTime, 0);
 
-		if ((Input.GetAxis ("Grab")) > 0.5) {
-			Debug.Log("Grabing" + Input.GetAxis("Grab"));
+		if (Input.GetAxis ("P1_Grab") > 0.5)
+			{
+				Debug.Log("Grabing " + Input.GetAxis("Grab"));
+				canGrab = true;
+			}
 
-
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "" && canGrab == true)
+		{
+			canGrab = false;
 		}
-
 	}
 }
