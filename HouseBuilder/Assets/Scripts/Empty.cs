1039da0fc2d37fbd5c0d009f720e334e4ef7c7ec
Computer Science Guest 134 <zzz134@usask.ca>
2019-01-26 09:53:43 -0600

    Fixing issues with empty files not being shows

--- HouseBuilder/Assets/Scripts/Empty.cs
+++ HouseBuilder/Assets/Scripts/Empty.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Empty : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
