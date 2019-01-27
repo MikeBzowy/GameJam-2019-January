1939bfce4af31aeecd5f4c16d8714321bf28abc8
Mike Bzowy <mikebzowy@gmail.com>
2019-01-27 15:07:29 -0600

    score display

---
+++ HouseBuilder/Assets/Scripts/ScoreDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplayer : MonoBehaviour {
+	public Text atmosphere;
+	public Text safety;
+	public Text luxury;
+	public Text design;
+
+	public Text total;
+
+	public void showScore(PlayerScore p) {
+		atmosphere.text += " " + p.atmosphere;
+		safety.text += " " + p.safety;
+		luxury.text += " " + p.luxury;
+		design.text += " " + p.interior_design;
+
+		total.text += " " + p.totalScore;
+	}
+}
