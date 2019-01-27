9cbab1fd3c64d76bc1999002cc136634d5f6d6d8
Mike Bzowy <mikebzowy@gmail.com>
2019-01-27 12:35:21 -0600

    reverse commit : updated the ui scripts : a09797a

--- HouseBuilder/Assets/Scripts/UI.cs
+++ HouseBuilder/Assets/Scripts/UI.cs
@@ -1,41 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
 
-	public Text timerText;
-	public float TimeLeft;
-
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-
-		TimeLeft -= Time.deltaTime;
-
-		float t = TimeLeft;
-		string minutes = ((int)t / 60).ToString ("00");
-		string seconds = (t % 60).ToString ("00");
-		string miliseconds = ((int)(t * 100f) % 100).ToString("00");
-
-		timerText.text = minutes + ":" + seconds + ":" + miliseconds;
-
-		if (TimeLeft <= 0.0f) {
-			timerEnded ();
-			timerText.text = ("00:00:00");
-		}
-	}
-
-	void timerEnded()
-	{
-		//do stuff here
-		Debug.Log("Time Over!!!!");
+	void Update () {
+		
 	}
 }
