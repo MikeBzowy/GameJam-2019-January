ca5f0da224a182cab24b71f1ee725145d6d63745
Computer Science Guest 134 <zzz134@usask.ca>
2019-01-27 15:11:39 -0600

    Added Sounds

--- HouseBuilder/Assets/Scripts/NewGame.cs
+++ HouseBuilder/Assets/Scripts/NewGame.cs
@@ -15,10 +15,14 @@ public class NewGame : MonoBehaviour {
 	int three = 0;
 	int four = 0;
 
+	public AudioClip Grabsound;
+
+	private AudioSource source;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -33,21 +37,25 @@ public class NewGame : MonoBehaviour {
 			Debug.Log("P1 in game");
 			one = 1;
 			GameObject.Find("P1_Grey").SendMessage ("Triger");
+			source.PlayOneShot(Grabsound,40);
 		}
 		if ((p2 > 0.5f) && two != 1){
 			Debug.Log("P2 in game");
 			two = 1;
 			GameObject.Find("P2_Grey").SendMessage ("Triger");
+			source.PlayOneShot(Grabsound,40);
 		}
 		if ((p3 > 0.5f) && three != 1){
 			Debug.Log("P3 in game");
 			three = 1;
 			GameObject.Find("P3_Grey").SendMessage ("Triger");
+			source.PlayOneShot(Grabsound,40);
 		}
 		if ((p4 > 0.5f) && four != 1){
 			Debug.Log("P4 in game");
 			four = 1;
 			GameObject.Find("P4_Grey").SendMessage ("Triger");
+			source.PlayOneShot(Grabsound,40);
 		}
 	
 		if ((one + two + three + four) >= 3){
